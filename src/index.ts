9bc44417ee0f21124cb23730ee49158bef3049ca
yitzi <el3k6uwq@duck.com>
2023-05-28 18:33:58 +0300

    feat: Add a prompt template for generating product related company names
    This commit adds a prompt template that generates prompts for company names related to a given product. It then formats the prompt with a product variable and runs the OpenAI model using the formatted prompt. This is a useful addition that will enable generating relevant company names.

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,23 @@
 import * as dotenv from "dotenv";
 import { OpenAI } from "langchain";
+import { PromptTemplate } from "langchain";
 
 dotenv.config();
+
+const template = "What is a good name for a company that makes {product}?";
+const prompt = new PromptTemplate({
+  template: template,
+  inputVariables: ["product"],
+});
 
 const model = new OpenAI({
   modelName: "gpt-3.5-turbo",
   openAIApiKey: process.env.OPENAI_API_KEY,
 });
 
-const res = await model.call(
-  "What's a good idea for an application to build with GPT-3?"
-);
+const formattedPrompt = await prompt.format({
+  product: "shoes",
+});
 
+const res = await model.call(formattedPrompt);
 console.log(res);
