e4f82dda13aa5f760c38ff3378b9fa02807ef986
yitzi <el3k6uwq@duck.com>
2023-05-29 17:25:37 +0300

    feat: Add `run` function that logs translated chat messages
    This commit adds an exported `run` function that instantiates a `ChatPromptTemplate` for translating messages between English and French. The `run` function then formats a prompt value using the chat template and logs the translated message to the console.

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,21 @@
+import { PromptTemplate } from "langchain";
 import { ChatPromptTemplate, HumanMessagePromptTemplate, SystemMessagePromptTemplate } from "langchain/prompts";
 
-const chatPrompt = ChatPromptTemplate.fromPromptMessages([
-  SystemMessagePromptTemplate.fromTemplate("You are a helpful assistant that translates {input_language} to {output_language}."),
-  HumanMessagePromptTemplate.fromTemplate("{text}"),
-]);
+export const run = async () => {
+  const prompt = ChatPromptTemplate.fromPromptMessages([
+    SystemMessagePromptTemplate.fromTemplate(
+      "You are a helpful assistant that translates {input_language} to {output_language}."),
+    HumanMessagePromptTemplate.fromTemplate("{text}"),
+  ]);
 
-const response = await chatPrompt.formatPromptValue({
-  input_language: "English",
-  output_language: "French",
-  text: "Hello, how are you?",
-});
-const messages = response.toChatMessages();
+  const response = await prompt.formatPromptValue({
+    input_language: "English",
+    output_language: "French",
+    text: "Hello, how are you?",
+  });
 
-console.log({messages});
+  const responseString = response.toString();
+  console.log(responseString);
+}
+
+run();
