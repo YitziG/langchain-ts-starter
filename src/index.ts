57138a1adb4eee6ca4f05aa550989de88e2f534c
yitzi <el3k6uwq@duck.com>
2023-05-29 16:59:15 +0300

    refactor: Update prompts in index.ts
    This commit updates the prompts used in index.ts to reflect changes in the langchain library. The `PromptTemplate` import was replaced with individual imports for `ChatPromptTemplate`, `HumanMessagePromptTemplate`, and `SystemMessagePromptTemplate`. The example prompt was also updated to a chat prompt, with two messages, one from the system and one from a human. The response is formatted using the chat prompt and then converted to chat messages.

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,15 @@
-import { PromptTemplate } from "langchain";
+import { ChatPromptTemplate, HumanMessagePromptTemplate, SystemMessagePromptTemplate } from "langchain/prompts";
 
-const prompt = PromptTemplate.fromTemplate(
-  "What is a good name for a company that makes {product}?"
-);
-const responseB = await prompt.format({ product: "colorful socks" });
-console.log({ responseB });
+const chatPrompt = ChatPromptTemplate.fromPromptMessages([
+  SystemMessagePromptTemplate.fromTemplate("You are a helpful assistant that translates {input_language} to {output_language}."),
+  HumanMessagePromptTemplate.fromTemplate("{text}"),
+]);
+
+const response = await chatPrompt.formatPromptValue({
+  input_language: "English",
+  output_language: "French",
+  text: "Hello, how are you?",
+});
+const messages = response.toChatMessages();
+
+console.log({messages});
