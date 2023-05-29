5b0288a33844b034604a61a818f1d86217c0704c
yitzi <el3k6uwq@duck.com>
2023-05-29 16:46:31 +0300

    Refactor the LLM API in index.ts to use a Prompt Template and make a formatted call.

--- src/index.ts
+++ src/index.ts
@@ -1,50 +1,7 @@
-import { LLMResult } from "langchain/schema";
-import { OpenAI } from "langchain/llms";
+import { PromptTemplate } from "langchain";
 
-// We can pass in a list of CallbackHandlers to the LLM constructor to get callbacks for various events.
-const model = new OpenAI({
-});
-
-model.callbackManager.handleLLMStart = async (llm: { name: string }, prompts: string[]) => {
-  console.log(JSON.stringify(llm, null, 2));
-  console.log(JSON.stringify(prompts, null, 2));
-}
-
-model.callbackManager.handleLLMEnd = async (output: LLMResult) => {
-  console.log(JSON.stringify(output, null, 2));
-}
-
-model.callbackManager.handleLLMError = async (err: Error) => {
-  console.error(err);
-}
-
-
-await model.call(
-  "What would be a good company name a company that makes colorful socks?"
+const prompt = PromptTemplate.fromTemplate(
+  "What is a good name for a company that makes {product}?"
 );
-// {
-//     "name": "openai"
-// }
-// [
-//     "What would be a good company name a company that makes colorful socks?"
-// ]
-// {
-//   "generations": [
-//     [
-//         {
-//             "text": "\n\nSocktastic Splashes.",
-//             "generationInfo": {
-//                 "finishReason": "stop",
-//                 "logprobs": null
-//             }
-//         }
-//     ]
-//  ],
-//   "llmOutput": {
-//     "tokenUsage": {
-//         "completionTokens": 9,
-//          "promptTokens": 14,
-//          "totalTokens": 23
-//     }
-//   }
-// }
+const responseB = await prompt.format({ product: "colorful socks" });
+console.log({ responseB });
